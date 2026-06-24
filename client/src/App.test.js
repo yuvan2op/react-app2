299924b8a94aca315ba7e195a5fbe3baaaa13d45
Yuvi <yuvan2op@gmail.com>
2026-06-24 15:52:13 +0530

    ci-cd gh actions

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,10 +1,9 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders app title', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  expect(screen.getByText(/React App 2 · Playground/i)).toBeInTheDocument();
 });
 
 
